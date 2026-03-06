922c1eb6ff40243e8f7503f434cb6005fbbf0e7e
Alice <malyefx@gmail.com>
2026-03-06 11:44:59 -0300

    first commit

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -7,7 +7,7 @@ import {
 import { Button } from "@/components/ui/button";
 import TypingTag from "@/components/layout/TypingTag";
 import type { Hobby } from "@/types";
-import perfilImg from "@/assets/images/minha-foto.jpg";
+import perfilImg from "@/assets/images/foto-perfil.jpg";
 
 const hobbies: Hobby[] = [
   { id: 1,  icon: <TerminalSquare size={20} />, label: "Liu",                              position: "-top-4 left-[10%]",      delay: 0.1 },
